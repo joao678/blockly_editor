20fa9fa24e765f50626393f32a9e7daad6e62a86
João Vitor Fernandes Canuto <joao678@gmail.com>
2022-06-10 19:41:01 -0300

    first

---
+++ index.js
@@ -0,0 +1,26 @@
+import Blockly from 'blockly';
+import DarkTheme from '@blockly/theme-dark';
+import toolbox from './toolbox.js';
+
+Blockly.inject('blockly_container', {
+    theme: DarkTheme,
+    sounds: false,
+    scrollbars: true,
+    trashcan: true,
+    renderer: 'zelos',
+    zoom: {
+        controls: true,
+        wheel: true,
+        startScale: 1.0,
+        maxScale: 3,
+        minScale: 0.3,
+        scaleSpeed: 1.2,
+        pinch: true
+    },
+    grid: {
+        spacing: 20,
+        length: 3,
+        colour: '#ccc',
+    },
+    toolbox: toolbox
+})
